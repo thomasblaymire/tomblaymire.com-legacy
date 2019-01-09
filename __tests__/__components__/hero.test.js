c4f14c4a7bccec61830d4664eda2248f443f1426
Thomas Blaymire <thomas.blaymire@skybettingandgaming.com>
2019-01-09 16:19:48 +0000

    Test setup

--- __tests__/__components__/hero.test.js
+++ __tests__/__components__/hero.test.js
@@ -1,4 +1,5 @@
 import { shallow } from 'enzyme';
+import { Jest } from 'jest';
 import * as React from 'react';
 import toJson from 'enzyme-to-json';
 import Hero from '../../components/Hero';
@@ -9,7 +10,7 @@ describe('The Hero Component', () => {
 
     it('Renders the Hero component', () => {
         const renderedComponent = renderComponent({});
-        expect()
+        expect(renderedComponent).toMatchSnapshot();
     });
 
     expect(toJson(renderedComponent)).toMatchSnapshot();
